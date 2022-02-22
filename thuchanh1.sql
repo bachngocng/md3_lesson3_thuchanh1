0064b7d53e447e47bdb45a226b9f0a68bf758808
bachngocng <nnbach97@gmail.com>
2022-02-22 13:33:07 +0700

    bai thuc hanh 2

--- thuchanh1.sql
+++ thuchanh1.sql
@@ -22,4 +22,22 @@ VALUES (1, 1, 8, 1),
 
 delete from mark;
 
+select * from student where status = true;
+
+select * from subject where credit <10;
+
+select S.StudentId, S.StudentName, C.ClassName
+From Student S join Class C on S.ClassId = C.ClassID;
+
+SELECT S.StudentId, S.StudentName, C.ClassName
+from Student S join Class C on S.ClassId = C.ClassID 
+where ClassName = 'A1';
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId;
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
+
 
